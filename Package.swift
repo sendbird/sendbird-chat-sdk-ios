9a5399231951a353107dd84475768a798947a529
Tez Park <tez.park@sendbird.com>
2025-02-21 16:18:21 +0900

    updated swift-tools-version to 5.10

--- Package.swift
+++ Package.swift
@@ -1,5 +1,5 @@
 
-// swift-tools-version:5.3
+// swift-tools-version:5.10
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 import PackageDescription
 let package = Package(

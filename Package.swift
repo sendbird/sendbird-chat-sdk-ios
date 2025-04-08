0ed783c05d9d16dd731e121ea3e36b31c8a32720
Tez Park <tez.park@sendbird.com>
2025-04-08 16:52:46 +0900

    Updated version to 4.25.3-beta.1

--- Package.swift
+++ Package.swift
@@ -1,5 +1,5 @@
 
-// swift-tools-version:5.3
+// swift-tools-version:5.10
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 import PackageDescription
 let package = Package(
@@ -15,8 +15,8 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "SendbirdChatSDK",
-            url: "https://github.com/sendbird/sendbird-chat-sdk-ios/releases/download/4.25.3/SendbirdChatSDK.xcframework.zip",
-            checksum: "c75a35cc147eb7ae0554646da5e4375892cb418db53f9c736963965fa16bdbac"
+            url: "https://github.com/sendbird/sendbird-chat-sdk-ios/releases/download/4.25.3-beta.1/SendbirdChatSDK.xcframework.zip",
+            checksum: "d54a492e13fba89f76617e844ebc9dffbcdf04b4591322cfab169ee657c94ade"
         ),
     ]
 )

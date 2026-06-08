51be077d491d99546b78b6a085b57c2442554306
Anka <sha.sdk_deployment@sendbird.com>
2026-06-08 07:52:34 +0000

    Release - 4.39.5

--- Package.swift
+++ Package.swift
@@ -13,13 +13,13 @@ let package = Package(
         ),
     ],
     dependencies: [
-        .package(url: "https://github.com/sendbird/sendbird-auth-ios.git", .exact("1.1.2"))
+        .package(url: "https://github.com/sendbird/sendbird-auth-ios.git", .exact("1.1.3"))
     ],
     targets: [
         .binaryTarget(
             name: "SendbirdChatSDK",
-            url: "https://github.com/sendbird/sendbird-chat-sdk-ios/releases/download/4.39.4/SendbirdChatSDK.xcframework.zip",
-            checksum: "d5124e647f08082309a39cdf6b7df20a9c5e524e9ef5c63829af52b6d6793ea6"
+            url: "https://github.com/sendbird/sendbird-chat-sdk-ios/releases/download/4.39.5/SendbirdChatSDK.xcframework.zip",
+            checksum: "c36402c0d61cc42f07c62e931d943847c4b8742b5e8cca7b6fcfccb4474964a3"
         ),
         .target(
             name: "SendbirdChatSDKWrapper",
